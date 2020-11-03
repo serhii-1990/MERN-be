58d925f83d6b3dfa18f954d469aa6d81c9bc72ba
s.tymchak <2freemangordon3@gmail.com>
2020-11-04 00:22:01 +0200

    signing users set.

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const fs = require("fs");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 
-const placesRoutes = require("./routes/places-routs");
-const usersRoutes = require("./routes/users-routs");
-const HttpError = require("./models/http-error");
-const Secrets = require("./secrets");
+const placesRoutes = require('./routes/places-routs');
+const usersRoutes = require('./routes/users-routs');
+const HttpError = require('./models/http-error');
+const Secrets = require('./secrets');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/uploads/images", express.static(path.join("uploads", "images")));
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-Width, Content-Type, Accept, Authorization"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-Width, Content-Type, Accept, Authorization'
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
   next();
 });
 
-app.use("/api/places", placesRoutes);
+app.use('/api/places', placesRoutes);
 
-app.use("/api/users", usersRoutes);
+app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
+  const error = new HttpError('Could not find this route.', 404);
   throw error;
 });
 
@@ -45,7 +45,7 @@ app.use((error, req, res, next) => {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({ message: error.message || "An unknow error occured!" });
+  res.json({ message: error.message || 'An unknow error occured!' });
 });
 
 mongoose
